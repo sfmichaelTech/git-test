b06f617d652b33d2851f90238f222b3efe6d39db
michael <1466813761@qq.com>
2019-06-11 23:14:23 +0800

    添加一个求和方法

--- Git_test_project/src/cn/itcast/git/GitTest.java
+++ Git_test_project/src/cn/itcast/git/GitTest.java
@@ -4,4 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("这是一个git项目");
     }
+
+    public int add(int a , int b){
+        return a+b;
+    }
 }
