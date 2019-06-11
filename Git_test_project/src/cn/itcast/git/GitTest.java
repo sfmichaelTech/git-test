ffa30fe089ff16e382562a13a9649a819861c8d8
michael <1466813761@qq.com>
2019-06-11 23:25:55 +0800

    修改第一版本

--- Git_test_project/src/cn/itcast/git/GitTest.java
+++ Git_test_project/src/cn/itcast/git/GitTest.java
@@ -3,9 +3,11 @@ package cn.itcast.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("这是一个git项目");
+        //hello test
     }
 
     public int add(int a , int b){
+        int i = a+b;
         return a+b;
     }
 }
